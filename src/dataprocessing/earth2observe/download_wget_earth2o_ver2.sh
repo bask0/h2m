f1d1681385b17935d516598b3861e93e3bbe4886
Basil Kraft <29065166+bask0@users.noreply.github.com>
2021-05-12 17:32:39 +0200

    contained (not so secret) information to dl E2O

--- src/dataprocessing/earth2observe/download_wget_earth2o_ver2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-models='uu ceh jrc cnr metfr eth csiro unikassel ecmwf'
-models='ecmwf'
-for mod in $models;do
-mkdir -p /Net/Groups/BGI/people/skoirala/Data/EartH2O/ver2/$mod
-wget ftp://e2o_guest:oowee3WeifuY1aeb@wci.earth2observe.eu/data/primary/public/$mod/wrr2/*.nc -P /Net/Groups/BGI/people/skoirala/Data/EartH2O/ver2/$mod/
-done
